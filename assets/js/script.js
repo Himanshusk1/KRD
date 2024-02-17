3df657819301b0db1ae374b15fa1ebd7eacb488b
Anurag Vishwakarma <anuraga2005@gmail.com>
2024-02-17 23:35:38 +0530

    Simple Design.

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+let reachNums = document.getElementsByClassName('reached-nums-data');
+let isAlreadyScrolled = false;
+
+const loadAllReachedValues = async()=> {
+    for(let i = 0; i < reachNums.length; i++) {
+        let e = reachNums[i], inVal = 0;
+        let reachVal = e.getAttribute('data-reachedNum');
+        let incVal = Math.round(reachVal/20);
+    
+        let intervals = setInterval(() => {
+            if(inVal <= reachVal) {
+                e.innerHTML = inVal+=incVal;
+            } else {
+                e.innerHTML = reachVal + "+";
+                clearInterval(intervals);
+            }
+        }, 50);
+    }
+    isAlreadyScrolled = true;
+}
+
+window.onscroll = ()=> {
+    if(!isAlreadyScrolled) {
+        loadAllReachedValues();
+    }
+}
---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gurukul Dream Foundation</title>
+    <meta name="description" content="Welcome to Gurukul Dream Foundation, where we empower the youth of tier 2/3 cities. Our goal is to provide tailored support and opportunities for young individuals to thrive. Through workshops, internships, and more, we aim to equip them with the skills they need to succeed. Join us as we shape a brighter future for every aspiring young mind.">
+    <meta name="keywords" content="Gurukul Dream Foundation, where we empower the youth of tier 2/3 cities. Our goal is to provide tailored support and opportunities for young individuals to thrive. Through workshops, internships, and more, we aim to equip them with the skills they need to succeed. Join us as we shape a brighter future for every aspiring young mind.">
+    <link rel="icon" href="./favicon.ico" type="image/x-icon">
+    <link href="./assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body class="overflow-x-hidden">
+    <?php include "_header.php"?>
+
+    <main class="container-auto ">
+        <!-- image slider section -->
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active" data-bs-interval="1000">
+            <img src="https://citiwebcast.web.app/favicon.ico" class="d-block w-100 main-image-slider" alt="...">
+            </div>
+            <div class="carousel-item" >
+            <img src="https://musiconi.web.app/assets/musiconi-logo-KfjCfDrd.png" class="d-block w-100 main-image-slider" alt="...">
+            </div>
+            <div class="carousel-item">
+            <img src="https://musiconi.web.app/assets/musiconi-logo-KfjCfDrd.png" class="d-block w-100 main-image-slider" alt="...">
+            </div>
+        </div>
+        </div>
+        <!-- image slider section -->
+
+        <div class="w-100 reached-numbers bg-black text-white d-flex flex-column flex-lg-row flex-wrap justify-content-evenly align-items-center px-2 py-0 ">
+        <div class="bg-dark reached-numbers-boxes text-center h3 p-4 rounded mt-2 text-wrap">
+                <span class="text-white">Our </span>
+                <span class="text-warning">Reach</span>
+        </div>
+        <div class="all-reached-data d-flex justify-content-evenly align-items-center flex-wrap flex-md-nowrap gap-md-4 gap-1 w-75  px-md-0 px-lg-2 overflow-x-scroll">
+
+            <div class="bg-black reached-numbers-boxes w-100 p-3 text-center d-flex flex-column align-items-center justify-content-center ">
+                <div class="reached-nums-data text-warning" data-reachedNum="10">
+                    0
+                </div>
+                <div class="reached-nums-about">
+                    District Level Events
+                </div>
+            </div>
+            <div class="bg-black reached-numbers-boxes w-100 px-3 py-1  text-center d-flex flex-column align-items-center justify-content-center ">
+                <div class="reached-nums-data text-warning" data-reachedNum="20">
+                    0
+                </div>
+                <div class="reached-nums-about">
+                    Startup Summits/ Sessions
+                </div>
+            </div>
+            <div class="bg-black reached-numbers-boxes w-100 p-3 text-center d-flex flex-column align-items-center justify-content-center ">
+                <div class="reached-nums-data text-warning" data-reachedNum="80">
+                    0
+                </div>
+                <div class="reached-nums-about">
+                    Workshops
+                </div>
+            </div>
+            <div class="bg-black reached-numbers-boxes w-100 p-3 text-center d-flex flex-column align-items-center justify-content-center ">
+                <div class="reached-nums-data text-warning" data-reachedNum="15">
+                    0
+                </div>
+                <div class="reached-nums-about">
+                    Industry Visits
+                </div>
+            </div>
+            <div class="bg-black reached-numbers-boxes w-100 p-3 text-center d-flex flex-column align-items-center justify-content-center ">
+                <div class="reached-nums-data text-warning" data-reachedNum="60">
+                    0
+                </div>
+                <div class="reached-nums-about">
+                Jobs provided
+                
+            </div>
+        </div>
+        <div class="bg-black reached-numbers-boxes w-100 p-3 text-center d-flex flex-column align-items-center justify-content-center ">
+            <div class="reached-nums-data text-warning"  data-reachedNum="100">
+                0
+            </div>
+            <div class="reached-nums-about">
+                Internships provided
+                
+            </div>
+        </div>
+        <div class="bg-black reached-numbers-boxes w-100 p-3 text-center d-flex flex-column align-items-center justify-content-center ">
+            <div class="reached-nums-data text-warning" data-reachedNum="10000">
+                0
+            </div>
+            <div class="reached-nums-about" >
+                Career Counselling Sessions through सफरनामा
+            </div>
+        </div>
+    </div>
+    </div>
+    
+    
+</main>
+
+    
+
+    <?php include "_footer.php"?>
+    <script src="./assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="./assets/js/script.js" ></script>
+</body>
+</html>
